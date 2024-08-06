c8c7328c5cd26106f73e3d5fcbeb29ff8b8c9451
root <rahul.soshte4771@gmail.com>
2024-08-06 21:58:37 +0000

    aggregation by tdep

---
+++ zephyr-fee-agg/src/lib.rs
@@ -0,0 +1,134 @@
+use zephyr_sdk::{soroban_sdk::xdr::{FeeBumpTransactionInnerTx, Operation, OperationBody, TransactionEnvelope, TransactionResultResult}, Condition, DatabaseDerive, DatabaseInteract, EnvClient};
+use ta::{indicators::ExponentialMovingAverage, Next};
+use zephyr_sdk::bincode;
+use zephyr_sdk::ZephyrVal;
+
+#[derive(DatabaseDerive, Clone)]
+#[with_name("avgfee")]
+pub struct Stats {
+    pub classic: i128,
+    pub contracts: i128,
+    pub other: i128,
+    pub fee_sor: ExponentialMovingAverage,
+    pub fee_clas: ExponentialMovingAverage,
+}
+
+const PERIOD: usize = 12 * 60 * 24;
+
+// Slightly updated version for precision correctness.
+// Please note that this design is not the most efficient (and it hasn't been thought through much) and can definitely be improved. This
+// is the result of an on-the-fly coded program in the Stellar Event "Working with Data on Stellar, the Role of Indexers and Live-Coding a ZephyrVM Program".
+#[no_mangle]
+pub extern "C" fn on_close() {
+    let env = EnvClient::new();
+    let reader = env.reader();
+
+    let (contract, classic, other, avg_soroban, avg_classic) = {
+        let mut contract_invocations = 0;
+        let mut classic = 0;
+        let mut other_soroban = 0;
+        let mut tot_soroban_fee = 0;
+        let mut tot_classic_fee = 0;
+
+        let envelopes = reader.envelopes_with_meta();
+        let mut successful_envelopes = 0;
+        
+        for (envelope, meta) in &envelopes {
+            let charged = meta.result.result.fee_charged;
+
+
+            let success = match meta.result.result.result {
+                TransactionResultResult::TxSuccess(_) => true,
+                TransactionResultResult::TxFeeBumpInnerSuccess(_) => true,
+                _ => false
+            };
+
+            if success {
+                successful_envelopes += 1;
+                match envelope {
+                    TransactionEnvelope::Tx(v1) => {
+                        count_ops_and_fees(v1.tx.operations.to_vec(), charged, &mut classic, &mut contract_invocations, &mut other_soroban, &mut tot_soroban_fee, &mut tot_classic_fee)
+
+                    },
+                    TransactionEnvelope::TxFeeBump(feebump) => {
+                        let FeeBumpTransactionInnerTx::Tx(v1) = &feebump.tx.inner_tx;
+                        count_ops_and_fees(v1.tx.operations.to_vec(), charged, &mut classic, &mut contract_invocations, &mut other_soroban, &mut tot_soroban_fee, &mut tot_classic_fee)
+                    },
+                    TransactionEnvelope::TxV0(v0) => {
+                        count_ops_and_fees(v0.tx.operations.to_vec(), charged, &mut classic, &mut contract_invocations, &mut other_soroban, &mut tot_soroban_fee, &mut tot_classic_fee)
+                    }
+                }
+            }
+        };
+
+        (contract_invocations as i128, classic as i128, other_soroban as i128, tot_soroban_fee as f64 / (contract_invocations) as f64, tot_classic_fee as f64 / (successful_envelopes - contract_invocations) as f64)
+    };
+
+    let current = env.read::<Stats>();
+    if let Some(row) = current.last() {
+        let mut row = row.clone();
+
+        if avg_classic.is_normal() {
+            row.fee_clas.next(avg_classic as f64);
+        };
+
+        if avg_soroban.is_normal() {
+            row.fee_sor.next(avg_soroban as f64);
+        };
+
+        let previous_classic = row.classic;
+
+        row.classic += classic;
+        row.contracts += contract;
+        row.other += other;
+        env.update_inner(&row, &[Condition::ColumnEqualTo("classic".into(), bincode::serialize(&ZephyrVal::I128(previous_classic)).unwrap())])
+    } else {
+        let mut fee_soroban = ExponentialMovingAverage::new(PERIOD).unwrap();
+        
+        if avg_soroban.is_normal() {
+            fee_soroban.next(avg_soroban);
+        }
+
+        let mut fee_classic = ExponentialMovingAverage::new(PERIOD).unwrap();
+        if avg_classic.is_normal() {
+            fee_classic.next(avg_classic);
+        }
+
+        env.put(&Stats {
+            classic,
+            contracts: contract,
+            other,
+            fee_sor: fee_soroban,
+            fee_clas: fee_classic
+        })
+    }
+}
+
+fn count_ops_and_fees(ops: Vec<Operation>, txfee: i64, classic: &mut i32, contract_invocations: &mut i32, other_soroban: &mut i32, tot_soroban_fee: &mut i64, tot_classic_fee: &mut i64) {
+    // only 1 invokehostfn operations can be in one transaction
+    if let Some(op) = ops.get(0) {
+        if let OperationBody::InvokeHostFunction(_) = op.body {
+            *tot_soroban_fee += txfee;
+        } else {
+            *tot_classic_fee += txfee
+        }
+    }
+    
+    for op in ops.iter() {
+        match op.body {
+            OperationBody::InvokeHostFunction(_) => {
+                *contract_invocations += 1;
+            },
+            OperationBody::ExtendFootprintTtl(_) => {
+                *other_soroban += 1;
+            },
+            OperationBody::RestoreFootprint(_) => {
+                *other_soroban += 1;
+            },
+            _ => {
+                *classic += 1;
+            }
+        }
+    }
+}
+

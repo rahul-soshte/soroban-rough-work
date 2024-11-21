647f2443a44460479ca9c23398e128663f164a75
rahul-soshte <rahul.soshte4771@gmail.com>
2024-11-22 00:30:29 +0530

    js update

--- test_horizon.js
+++ test_horizon.js
@@ -1,3 +1,5 @@
 import * as StellarSDK from '@stellar/stellar-sdk';
-const server = new StellarSDK.Horizon.Server("https://horizon-testnet.stellar.or/g");
+const server = new StellarSDK.Horizon.Server("https://horizon-testnet.stellar.org");
 console.log(server)
+const server2 = new StellarSDK.rpc.Server("https://soroban-testnet.stellar.org/");
+console.log(await server2.getTransaction("d71d3f90fced581c4d432b6626a5ea3baaec36130f784fc618e42c02ddbeb307"));
